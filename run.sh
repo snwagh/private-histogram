ca1c13127c7e7e90687213072f7d3bdfc311fa73
Sameer Wagh <snwagh@gmail.com>
2024-10-15 13:31:32 -0400

    Cleaning up

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running private-histogram app"
-
-# Activate virtual environment and run the application
-uv venv --allow-existing
-uv pip install --upgrade syftbox
-uv run python main.py
